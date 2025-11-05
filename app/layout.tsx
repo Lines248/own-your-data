117ed77844637f07b9068b60887e132818648e77
Lines <lines.sultzer@gmail.com>
2025-11-04 20:12:02 -0500

    optimized javascript

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+</head>
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
